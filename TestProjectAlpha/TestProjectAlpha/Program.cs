a312a626f7147f6c1041ec5e77a312a10365bcca
zaboltar <cletothar@gmail.com>
2018-08-14 13:13:30 -0500

    otro avance a ciegas

--- TestProjectAlpha/TestProjectAlpha/Program.cs
+++ TestProjectAlpha/TestProjectAlpha/Program.cs
@@ -7,7 +7,17 @@ using System.Threading.Tasks;
 namespace TestProjectAlpha {
     class Program {
         static void Main(string[] args) {
+            string hiddenWord = Console.ReadLine();
+            string shownWord = "";
 
+            Console.Clear();
+
+            for (int i = 0, i < hiddenWord.Length, i++) {
+                shownWord += '*'
+            }
+
+           Console.WriteLine(shownWord);
+            
         }
     }
 }
