ffd499199e5598035ece4768ecca0ca1ecf50b03
zaboltar <cletothar@gmail.com>
2018-09-07 03:36:42 -0500

    made orthoCam+ maze 98%

---
+++ CletoThar/Assets/Scripts/CameraController.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+public Transform target;
+public float smoothSpeed = 0.25f;
+
+public Vector3 offset;
+
+		void LateUpdate () {
+			transform.position = target.position + offset;
+	}
+
+}
