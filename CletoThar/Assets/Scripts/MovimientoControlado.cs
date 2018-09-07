7d1bee398b49def0e55492c2d0ddf60ffd877430
zaboltar <cletothar@gmail.com>
2018-09-07 02:08:30 -0500

    develop unity movement+maze

--- CletoThar/Assets/Scripts/MovimientoControlado.cs
+++ CletoThar/Assets/Scripts/MovimientoControlado.cs
@@ -3,21 +3,36 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class MovimientoControlado : MonoBehaviour {
-    public float speed = 2;
+    public float speed = 5;
 
   	// Use this for initialization
 	void Start () {
-                    
-	}
+    
+
+    // por si se desconfigura
+		// this.Rigidbody.useGravity = false;
+	}		// pero no se como usarlo :p
 	
 	// Update is called once per frame
 	void Update () {
+
+		//código de mov simple vertical
         if(Input.GetKey (KeyCode.UpArrow)) {
             transform.position += Vector3.up * Time.deltaTime * speed ;
         } else if (Input.GetKey (KeyCode.DownArrow))
         {
             transform.position += Vector3.down * Time.deltaTime * speed;
         }
+
+        //código de mov simple horizontal
+        if(Input.GetKey (KeyCode.LeftArrow)) {
+            transform.position += Vector3.left * Time.deltaTime * speed ;
+        } else if (Input.GetKey (KeyCode.RightArrow))
+        {
+            transform.position += Vector3.right * Time.deltaTime * speed;
+        }
 		
+
+
 	}
 }
