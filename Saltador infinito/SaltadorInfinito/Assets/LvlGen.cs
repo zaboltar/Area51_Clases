df9c828b34f936f88e6bfcaefb03de051a052d56
zaboltar <cletothar@gmail.com>
2018-09-08 17:09:53 -0500

    SaltadorInfinito Esbozado

---
+++ Saltador infinito/SaltadorInfinito/Assets/LvlGen.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LvlGen : MonoBehaviour {
+
+	public GameObject platformPrefab;
+
+	public int numberOfPlatforms = 200;
+	public float lvlWidth = 12f;
+	public float minY = .2f;
+	public float maxY = 1.5f;
+
+	// Use this for initialization
+	void Start () {
+
+		Vector3 spawnPosition = new Vector3();
+		
+		for (int i=0; i < numberOfPlatforms; i++){
+		
+		spawnPosition.y += Random.Range(minY, maxY);
+		spawnPosition.x = Random.Range(-lvlWidth, lvlWidth);
+		Instantiate(platformPrefab, spawnPosition, Quaternion.identity);
+		}
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
