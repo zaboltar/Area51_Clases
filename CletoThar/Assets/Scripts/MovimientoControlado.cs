1956ae23404238ab114538c82da85b63363f8049
zaboltar <cletothar@gmail.com>
2018-09-11 13:05:44 -0500

    movement things //script apuntado

--- CletoThar/Assets/Scripts/MovimientoControlado.cs
+++ CletoThar/Assets/Scripts/MovimientoControlado.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class MovimientoControlado : MonoBehaviour {
     public float speed = 5;
+    public float angSpeed = 20;
 
   	// Use this for initialization
 	void Start () {
@@ -14,11 +15,19 @@ public class MovimientoControlado : MonoBehaviour {
 	}		// pero no se como usarlo :p
 	
 	// Update is called once per frame
+
+    // si usamos transform.up el mov es relativo al obj
+    // si usamos vector3.up nos refiere a los ejes globales!!
 	void Update () {
 
 		//código de mov simple vertical
         if(Input.GetKey (KeyCode.UpArrow)) {
+
+            // b/ transform.Translate (Vector3.forward * Time.deltaTime * speed ;
             transform.position += Vector3.up * Time.deltaTime * speed ;
+
+            // aca Hilble cambia la cam a zenital y usa vector3.forward y vect3.back
+
         } else if (Input.GetKey (KeyCode.DownArrow))
         {
             transform.position += Vector3.down * Time.deltaTime * speed;
@@ -32,7 +41,22 @@ public class MovimientoControlado : MonoBehaviour {
             transform.position += Vector3.right * Time.deltaTime * speed;
         }
 		
-
+        // tank rotation - considerar camara y desde donde se rota
+        // if (Input.GetKey (KeyCode.LeftArrow)) { transform.Rotate (Vector3.down * Time.deltaTime * angSpeed );}
 
 	}
+
+
+
+    // void Translate (Vector3 vector){
+       // transform.position += vector;
+        // y añadir /b/ aunque ya vuelve este metodo obsoleto, unity lo incluye
+   // }
+
+    // antes se podia acceder directamente al rigidbody...ahora es deprecated, legacycode
+    // Llamar al componente: GetComponent<ofType>
+    // pedir transform: GetComponent<Transform>() ... = a transform con minuscula
+    // para Rigid-body-. GetComponent<Rigidbody>().velocity = Vector3.left * speed;
+    // tmb existe para arrays: GetComponentSSSSS => devuelve array
+    // 
 }
