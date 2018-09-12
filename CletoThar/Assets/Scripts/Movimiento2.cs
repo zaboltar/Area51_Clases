5bfd1f30f8df7876bc6d6e673a4e12b8338a6fbb
zaboltar <cletothar@gmail.com>
2018-09-12 13:49:49 -0500

    add expo lebinz

---
+++ CletoThar/Assets/Scripts/Movimiento2.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movimiento2 : MonoBehaviour {
+
+    public float speed = 5;
+
+  	// Use this for initialization
+	void Start () {
+
+	}
+    
+	void Update () {
+
+		//código de mov simple vertical
+        if(Input.GetKey (KeyCode.UpArrow)) {
+            transform.Translate (Vector3.forward * Time.deltaTime * speed);
+            } else if (Input.GetKey (KeyCode.DownArrow))
+        {
+             transform.Translate (Vector3.back * Time.deltaTime * speed);
+        }
+
+        //código de mov simple horizontal
+        if(Input.GetKey (KeyCode.LeftArrow)) {
+            transform.position += Vector3.left * Time.deltaTime * speed;
+        } else if (Input.GetKey (KeyCode.RightArrow)) {
+            transform.position += Vector3.right * Time.deltaTime * speed;
+        }
+		      
+	}
+
+ 
+}
