bc9cde4305454f6675ef9986fa5fa024d7ec6245
zaboltar <cletothar@gmail.com>
2018-09-18 19:37:34 -0500

    added Gol event

---
+++ CletoThar/Assets/Scripts/Gol.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gol : MonoBehaviour {
+
+	void OnTriggerEnter (Collider other) {
+		if (other.CompareTag("BALL")){
+			Debug.Log ("GOOOOL!");
+		}
+	}
+}
