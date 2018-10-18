b64a177d9443bd3fe8296c256350b7b260dd582a
zaboltar <cletothar@gmail.com>
2018-10-18 13:03:39 -0500

    va a explotar

--- CletoThar2D/Assets/Platformer/Scripts/PhysicsPlatformMov2D.cs
+++ CletoThar2D/Assets/Platformer/Scripts/PhysicsPlatformMov2D.cs
@@ -10,10 +10,22 @@ public class PhysicsPlatformMov2D : MonoBehaviour {
     public List<AxisPair> axes;
     public Rigidbody2D rb2D;
     public float gravity = 8;
+    public float jumpForce = 7f;
     public bool grounded;
     public Animator animator; //anim test
+    public Collider2D col2D;
 
     Vector3 movement;
+
+    Vector2 distanceLeft;
+    Vector2 distanceRight;
+    const float margin = 0.2f;
+    const float minDistance = 0.1f; //0.05 || 0.5f
+    public Vector2 pointLeft { get { return rb2D.position + distanceLeft; }}
+    public Vector2 pointRight { get { return rb2D.position + distanceRight; }}
+
+
+
 
     void Reset () {
         rb2D = GetComponent<Rigidbody2D> ();
@@ -23,7 +35,11 @@ public class PhysicsPlatformMov2D : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		
+        distanceLeft.x = -(col2D.bounds.extents.x - margin);
+        distanceLeft.y = -col2D.bounds.extents.y;
+        distanceRight.x = col2D.bounds.extents.x - margin;
+        distanceRight.y = -col2D.bounds.extents.y;
+
 	}
 	
 	// Update is called once per frame
@@ -77,22 +93,29 @@ public class PhysicsPlatformMov2D : MonoBehaviour {
         } else if (Input.GetKeyUp(KeyCode.Y)) {
           animator.SetBool ("Att5", false);
            }
+
+        if (grounded && Input.GetKeyDown(KeyCode.Space)) {
+            
+            speed.y = jumpForce;
+        } 
+
+
+
  }
     
 
     void FixedUpdate () {
         if (!grounded) {
             speed.y -= gravity * Time.fixedDeltaTime;
-        }
+        } 
 
-        RaycastHit2D hit2D = Physics2D.Raycast (transform.position, Vector3.down, 0.55f);
-        if (hit2D) {
-           // Debug.Log (hit2D.collider.name);
-            speed.y = 0;
-            grounded = true;
-        } else {
-            grounded = false;
-        }
+        GroundCheck();
+
+        SpriteRenderer rend = GetComponent<SpriteRenderer>();
+        if (movement.x > 0 && rend.flipX) { rend.flipX = false; }
+        else if (movement.x < 0 && !rend.flipX) { rend.flipX = true; }
+
+
 
         movement = movement.normalized * speed.x * Time.fixedDeltaTime;
         movement.y = speed.y * Time.fixedDeltaTime;
@@ -101,6 +124,32 @@ public class PhysicsPlatformMov2D : MonoBehaviour {
 
     void OnDrawGizmos () {
         Gizmos.color = Color.red;
-        Gizmos.DrawRay (transform.position, Vector3.down);
+        Gizmos.DrawRay (pointLeft, Vector3.down * minDistance);
+        Gizmos.DrawRay(pointRight, Vector3.down * minDistance); //0.1||0.5f
     }
+
+    void GroundCheck()
+    {
+        Vector2[] points = { pointLeft, pointRight };
+        RaycastHit2D[] hits2D = new RaycastHit2D[points.Length];
+        grounded = false;
+
+        for (int i = 0; i < points.Length; i++)
+        {
+            hits2D[i] = Physics2D.Raycast(points[i], Vector3.down, minDistance);
+
+            if (hits2D[i])
+            {
+                Debug.Log (hits2D[i].collider.name);
+                if (speed.y < 0){
+                    transform.position = (transform.position + (Vector3.down * minDistance));
+                    // Debug.Log algo
+                    speed.y = 0;
+                }
+                grounded = true;
+                break;
+            }
+        }
+    }
+
 }
