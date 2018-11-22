d3aac9a4a7888a4f4c812848ebb89124992894b5
zaboltar <cletothar@gmail.com>
2018-11-22 13:02:37 -0500

    platformBehaviour spices

--- CletoThar3D/CletoThar3D/Assets/Scripts/CharacterBaseMov3D.cs
+++ CletoThar3D/CletoThar3D/Assets/Scripts/CharacterBaseMov3D.cs
@@ -34,6 +34,14 @@ public class CharacterBaseMov3D : MonoBehaviour {
         if (grounded && Input.GetKeyDown(KeyCode.Space)) {
             rigBod.AddForce(Vector3.up * jumpForce, ForceMode.VelocityChange);
         }
+
+        if (rigBod.velocity.x != 0 || rigBod.velocity.z != 0) {
+            Vector3 temp = rigBod.velocity;
+            temp.x = Mathf.MoveTowards(temp.x, 0, 2f * Time.deltaTime);
+            temp.z = Mathf.MoveTowards(temp.z, 0, 2f * Time.deltaTime);
+            rigBod.velocity = temp;
+            Debug.Log(rigBod.velocity);
+        } //epsilon es un infinito a zero float.Epsilon
 	}
 
 	void FixedUpdate () {
@@ -58,20 +66,32 @@ public class CharacterBaseMov3D : MonoBehaviour {
         respawnData = transformData;
     }
 
-    /*float GetMaxInclination () {
+	/*float GetMaxInclination () {
         groundCollection.Sort((x, y) => y.incNormalized.CompareTo(x.incNormalized));
         return groundCollection.Count != 0 ? groundCollection[0].incNormalized : 0;
     }*/
 
+	void OnCollisionEnter(Collision collision) {
+        /*if (collision.collider.CompareTag("MovingPlatform")) {
+            transform.SetParent(collision.transform); 
+            //emparenta el player a la plataforma, hace falta desvincularlo!
+            // se moviò esta parte a otro lado
+        }*/
+	}
 
 	void OnCollisionStay (Collision collision) {
         if (!groundCollection.Contains(collision.collider)) {
             foreach (ContactPoint contact in collision.contacts) {
                 Debug.DrawRay(contact.point, contact.normal, Color.yellow, 0.25f);
                 float inclination;
-                if ((inclination = Vector3.Dot(contact.normal, Vector3.up)) > 0.85f) {
+                if ((inclination = Vector3.Dot(contact.normal, Vector3.up)) > 0.85f)
+                {
                     grounded = true;
                     groundCollection.Add(collision.collider);
+                }
+                if (collision.collider.CompareTag("MovingPlatform"))
+                {
+                    transform.SetParent(collision.transform);
                     break;
                 }
             }
@@ -80,10 +100,19 @@ public class CharacterBaseMov3D : MonoBehaviour {
     void OnCollisionExit(Collision collision) {
         if (groundCollection.Contains(collision.collider)) {
             groundCollection.Remove(collision.collider);
+            /*if (collision.collider.CompareTag("MovingPlatform")) {
+                transform.SetParent(null);
+            }*/ //lookAtThis
         }
         if (groundCollection.Count == 0) {
             grounded = false;
         }
+        if (collision.collider.CompareTag("MovingPlatform")) {
+                transform.SetParent(null);
+           Vector3 exitMomentum = collision.collider.GetComponent<PlatformBehaviour3D>().currentMomentum;
+            rigBod.AddForce(exitMomentum, ForceMode.VelocityChange);
+            }
+
     }
 
 
@@ -98,3 +127,11 @@ public class CharacterBaseMov3D : MonoBehaviour {
         Gizmos.DrawRay(transform.position, transform.forward);
 	}
 }
+
+
+
+
+// para saltos, hay que usar un blend tree, la condicion que lo activa es la negacion del isGrounded. Lo que se chequea es un valor
+// normalizado del velocity (un float), especificamente del Y. Una animacion es para elevaciòn, otra para caidas, eso evita los problemas de salto 
+// "estàtico"... Debe implementarse en algun update o mejor, fixed update. Meter la mano al velocity es delicado y es mejor usarlo solo en 
+// situaciones como para respawnear. Habria que asignar un booleano fijo y chequearlo constantemente desde el update...o algo asì! 
