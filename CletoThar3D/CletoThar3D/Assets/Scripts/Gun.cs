8ee017c2048af948fb2a76ae5736eec34fd512ad
zaboltar <cletothar@gmail.com>
2018-11-29 01:11:19 -0500

    added FPS scene

---
+++ CletoThar3D/CletoThar3D/Assets/Scripts/Gun.cs
@@ -0,0 +1,61 @@
+﻿
+using UnityEngine;
+
+public class Gun : MonoBehaviour {
+
+	public float damage = 10f;
+	public float range = 100f;
+	public float impactForce = 100f;
+	public float fireRate = 15f;
+	
+	public Camera fpsCam;
+	public ParticleSystem muzzleFlash;
+	public GameObject impactFX;
+
+	private float nextTimetoFire = 0f;
+
+	// Update is called once per frame
+	void Update () {
+		
+		if (Input.GetButton("Fire1") && Time.time >= nextTimetoFire) {
+
+			nextTimetoFire = Time.time + 1f/fireRate;
+			Shoot();
+
+		}
+
+	}
+
+
+	void Shoot() {
+
+		muzzleFlash.Play();
+
+		RaycastHit hit;
+
+
+
+		if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range) ) {
+			
+			Debug.Log(hit.transform.name);
+
+			Target target = hit.transform.GetComponent<Target>();
+
+			if (target != null) {
+				target.TakeDamage(damage);
+			}
+
+			if (hit.rigidbody != null) {
+				hit.rigidbody.AddForce(-hit.normal * impactForce);
+			}
+
+			GameObject impactGO = Instantiate(impactFX, hit.point, Quaternion.LookRotation(hit.normal));
+			Destroy(impactGO, 2f);
+		}
+		
+
+	}
+
+
+}
+ 
