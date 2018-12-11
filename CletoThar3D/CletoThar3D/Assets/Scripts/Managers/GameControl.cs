bbcdcb192825e915addbd00ffe39b798ff67465e
zaboltar <cletothar@gmail.com>
2018-12-11 13:03:44 -0500

    fadeIn fadeOut

---
+++ CletoThar3D/CletoThar3D/Assets/Scripts/Managers/GameControl.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class GameControl : MonoBehaviour {
+
+    // crearemos un singleton
+
+    static public GameControl instance;
+    public CharacterBaseMov3D currentPlayer;
+    bool inTransition;
+    public UIManager uIManager;
+
+	void Awake () {
+
+        if (instance == null ) {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+            } else {
+            if (instance != this) {
+                if (gameObject != instance.gameObject) {
+                    Destroy(gameObject);
+                } else {
+                    Destroy(this);
+                }
+            }
+        }
+
+	}
+	
+    public void StartRespawnProcess (){
+        if (!inTransition) {
+            StartCoroutine(RespawnProcess());
+            inTransition = true;
+        }
+
+    } 
+
+    IEnumerator RespawnProcess () {
+        yield return uIManager.FadeProcess(currentPlayer.Respawn);
+
+
+        while (!currentPlayer.grounded) {
+            yield return null;
+        }
+
+        //yield return new WaitUntil( () => currentPlayer.grounded);
+        currentPlayer.EnableInput();
+        inTransition = false;
+    }
+
+}
+
+// ver delegates, punteros a funciòn, punteros de memoria, commodore style
+// en vez de contener valores, tienes direccion del lugar en donde se almacena alguna valor
+// delegate es un espacio de memoria al que hace referencia cierta definicion para que otros metodos accedan a èl
+// deberia haber un transition manager que llame metodos del game controller .... -_-"
+// para invocar action el metodo debe ser un void y no tener paràmetros!, asi se puede pasar un metodo como parametro en otro metodo
+// 
---
+++ CletoThar3D/CletoThar3D/Assets/Scripts/Managers/UIManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+
+
+public class UIManager : MonoBehaviour {
+
+    public Image fadePanel;
+	
+    public IEnumerator FadeProcess(Action fadeCall) {
+
+        fadePanel.gameObject.SetActive(true);
+        fadePanel.enabled = true;
+
+        while (fadePanel.color.a != 1) {
+            MoveImageAlpha(fadePanel, 1, 2f * Time.deltaTime);
+            yield return null;  
+        }
+
+        //external desired method should run here!
+        fadeCall.Invoke();
+
+        while (fadePanel.color.a != 0)
+        {
+            MoveImageAlpha(fadePanel, 0, 2f * Time.deltaTime);
+            yield return null;
+        }
+
+        Debug.Log("Made it!");
+
+
+          
+    
+    }
+
+    void MoveImageAlpha (Image image, float targetAlpha, float speed) {
+        Color color = image.color;
+        color.a = Mathf.MoveTowards(image.color.a, targetAlpha, speed);
+        image.color = color;
+    }
+
+}
